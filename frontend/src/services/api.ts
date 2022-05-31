9a5503da379d34c4056a02ea70b58ed05f995c49
willianproni <wifepro@hotmail.com>
2022-05-30 21:14:38 -0300

    Atualizacoes

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000/admin/"
+    baseURL: "http://127.0.0.1:8000/"
 })
 
 export default api;
---
+++ frontend/src/services/suporte.ts
@@ -0,0 +1,8 @@
+import api from "./api";
+
+export async function RequestSuport() {
+    const response =
+        await api.post("suporte/", {
+            
+        })
+}
