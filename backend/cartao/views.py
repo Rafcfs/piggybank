a06f86a5b72bae4901024681a028614ddabebe8c
Rafcfs <98784154+Rafcfs@users.noreply.github.com>
2022-06-01 21:45:19 -0300

    Update views.py

--- backend/cartao/views.py
+++ backend/cartao/views.py
@@ -6,4 +6,5 @@ from .models import CartaoCredito
 
 class CartaoView(generics.CreateAPIView):
     queryset = CartaoCredito.objects.all()
+    permission_classes = [permissions.AllowAny]
     serializer_class = CartaoSerializer
