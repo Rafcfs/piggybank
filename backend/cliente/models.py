87791d98b93b76ce70ce48d6e18577c240aac986
rcfss <rafacarrasqueira@gmail.com>
2022-06-02 18:04:02 -0300

    commit

--- backend/cliente/models.py
+++ backend/cliente/models.py
@@ -63,7 +63,7 @@ class Pessoa_Fisica(Cliente):
     estado_civil = models.CharField(max_length=1, choices=ESTADOS_CIVIS)
 
     def __str__(self):
-        return self.cpf
+        return self.nome
 
     class Meta:
         verbose_name = 'Pessoa Física'
