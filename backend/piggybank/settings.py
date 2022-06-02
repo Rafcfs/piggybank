57e010eafd399cce3795668b1c47300f54df6bf8
willianproni <wifepro@hotmail.com>
2022-06-01 23:07:35 -0300

    ajustes erro rquest 403

--- backend/piggybank/settings.py
+++ backend/piggybank/settings.py
@@ -77,12 +77,9 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000',
-    'http://localhost:8000'
-)
+CORS_ORIGIN_WHITELIST = ['http://localhost:3000']
 
-CSRF_TRUSTED_ORIGINS = ['localhost:3000']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
 
 ROOT_URLCONF = 'piggybank.urls'
 
