0f274ae7af48613e879320440046afcfe4b6ae02
willianproni <wifepro@hotmail.com>
2022-06-02 17:23:50 -0300

    Tela de Cadastro

--- frontend/src/screens/MainPage/index.tsx
+++ frontend/src/screens/MainPage/index.tsx
@@ -11,7 +11,7 @@ export function MainPage() {
             <Header />
             <div>
                 <OptionsUser>
-                    <Link to="/SelectRegister"><ButtonMain>
+                    <Link to="/Cadastro"><ButtonMain>
                         Cadastro
                     </ButtonMain></Link>
                     <Link to="/login">
